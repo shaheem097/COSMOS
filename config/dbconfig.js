f97e3759b9fa0c9fb8b673151ecc23065f4a37ef
shaheem097 <shaheemsha097@gmail.com>
2023-12-19 15:44:11 +0530

    v17

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017/COSMOS',{useNewUrlParser:true})
+mongoose.connect('mongodb+srv://shaheemsha097:sha2251@cluster0.a7hgmee.mongodb.net/COSMOS',{useNewUrlParser:true})
 
 mongoose.connection.once('open',()=>console.log('database connection success')).on('error',error=>{
 console.log(error);
